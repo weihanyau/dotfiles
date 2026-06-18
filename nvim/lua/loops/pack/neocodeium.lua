7bb2828301caa4a7e2edc4338ec3989e8b9a0eaf
weihanyau <weihanyau@gmail.com>
2026-06-18 23:42:44 +0800

    auto neocodeium

--- nvim/lua/loops/pack/neocodeium.lua
+++ nvim/lua/loops/pack/neocodeium.lua
@@ -2,16 +2,16 @@ vim.pack.add({ "https://github.com/monkoose/neocodeium" })
 
 local neocodeium = require("neocodeium")
 neocodeium.setup({
-	manual = true,
+	manual = false,
 })
 vim.keymap.set("i", "<A-f>", neocodeium.accept)
 vim.keymap.set("i", "<A-e>", function()
 	neocodeium.cycle_or_complete()
 end)
 
-vim.api.nvim_create_autocmd("User", {
-	pattern = "NeoCodeiumCompletionDisplayed",
-	callback = function()
-		require("blink.cmp").hide()
-	end,
-})
+-- vim.api.nvim_create_autocmd("User", {
+-- 	pattern = "NeoCodeiumCompletionDisplayed",
+-- 	callback = function()
+-- 		require("blink.cmp").hide()
+-- 	end,
+-- })
