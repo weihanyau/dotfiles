bb04ef1071e15f2b5e45909abbff6c10a80a3c9f
weihanyau <weihanyau@gmail.com>
2026-06-22 11:53:20 +0800

    increase conform timeout

--- nvim/lua/loops/pack/lsp.lua
+++ nvim/lua/loops/pack/lsp.lua
@@ -69,7 +69,7 @@ require("conform").setup({
 	},
 	format_on_save = {
 		-- These options will be passed to conform.format()
-		timeout_ms = 500,
+		timeout_ms = 1000,
 		lsp_format = "fallback",
 	},
 })
